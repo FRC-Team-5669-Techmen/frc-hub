043e63722e0d6d020b213ced89cc7cd2742f6620
Noah <gamerpenguin2005@gmail.com>
2021-09-05 01:23:31 -0700

    sign in with redirect

--- public/js/auth/auth.js
+++ public/js/auth/auth.js
@@ -1,7 +1,7 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, browserLocalPersistence, setPersistence } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-auth.js";
+import { getAuth, getRedirectResult, signInWithRedirect, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged, browserLocalPersistence, setPersistence } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-auth.js";
 //import * as app from "../main.js"
 
-import {authChangeHandler} from "../main.js"
+import { authChangeHandler } from "../main.js"
 
 const provider = new GoogleAuthProvider();
 var auth;
@@ -12,13 +12,7 @@ export async function initAuth(app) {
     }
     authObjSet().then(() => {
         onAuthStateChanged(auth, (user) => {
-            if (user) {
-                authChangeHandler(true, user)
-                return user
-            } else {
-                authChangeHandler(false)
-                return false
-            }
+            authChangeHandler(!!user, user)
         });
     })
     provider.setCustomParameters({
@@ -50,31 +44,31 @@ export async function initPersist() {
         });
 }
 
-export function googleSignIn() {
+export async function googleSignIn() {
     if (authState()) {
         console.log(authState())
     } else {
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                // The signed-in user info.
-                const user = result.user;
-
-                console.log("User logged in: ")
-                console.log(user)
-                // ...
-            }).catch((error) => {
-                // Handle Errors here.
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // The email of the user's account used.
-                const email = error.email;
-                // The AuthCredential type that was used.
-                const credential = GoogleAuthProvider.credentialFromError(error);
-                // ...
-            });
+        signInWithRedirect(auth, provider)
+        /*signInWithPopup(auth, provider).then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+        
+            console.log("User logged in: ")
+            console.log(user)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });*/
     }
 
 }
@@ -88,16 +82,16 @@ export function signOutUser() {
 }
 
 export function authState() {
-    //onAuthStateChanged(auth, (user) => {
-    //    if (user) {
-    //        // User is signed in, see docs for a list of available properties
-    //        // https://firebase.google.com/docs/reference/js/firebase.User
-    //        const uid = user.uid;
-    //        return user
-    //    } else {
-    //        return false
-    //    }
-    //});
+    /*onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            const uid = user.uid;
+            return user
+        } else {
+            return false
+        }
+    });*/
     return auth.currentUser;
 }
 
