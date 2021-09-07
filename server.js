5c49fe79c54e19e6b47e444fbd37cd02e6efda6c
dazon <44048306+FakeDazon@users.noreply.github.com>
2021-09-07 16:22:39 -0700

    change port handling

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 
+var port = process.env.PORT || 3000;
+
 const app = express()
 
 const publicDir = path.join(__dirname, 'public')
@@ -10,4 +12,4 @@ app.use(express.static(__dirname + '/public/'));
 app.get('/', (req, res) => res.sendFile(path.join(publicDir, 'index.html')))
 app.get('/login', (req, res) => res.sendFile(path.join(publicDir, 'login.html')))
 
-app.listen(3000, () => console.log('Listening on port http://localhost:3000/'))
+app.listen(3000, () => console.log('Listening on port http://localhost:' + port + "/"))
